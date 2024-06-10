a779e065a98e4ece7a8cfa60516f78446975911f
ada taylor <admin@adataylor.io>
2024-06-09 22:11:30 -0500

    updated to use dicts

--- modDownloader.py
+++ modDownloader.py
@@ -8,14 +8,22 @@ MOD_DIR = "C:/ArmA 3/Arma 3 Server/"
 
 soup = BeautifulSoup(read_data, 'html.parser')
 
-steam = pysteamcmd.Steamcmd("C:/ArmA 3/ArmA 3 Server/")
+modListName = []
+modListID = []
+modDict = {}
 
-for itemL in soup.find_all('a'):
-	for modName in soup.find_all(attrs={"data-type" : "DisplayName"}):
-		editedModName = re.sub(r"[=\s_+-\.,\[\]\(\)']", "", modName.string)
+for modName in soup.find_all(attrs={"data-type" : "DisplayName"}):
+	editedModName = re.sub(r"[=\s_+-\.,\[\]\(\)']", "", modName.string)
+	modListName.append(editedModName)
 
-	for link in soup.find_all('a'):
-		mod = re.split(r"=", link.get('href'))[1]
 
-	os.system("C:/steamcmd/steamcmd.exe +force_install_dir C:/ArmA 3/Arma 3 Server +login anonymous +workshop_download_item 107410 {mod}")
+for link in soup.find_all('a'):
+	mod = re.split(r"=", link.get('href'))[1]
+	modListID.append(mod)
+
+for modKey in modListName:
+	modDict[modKey] = modListID
+
+for key in modDict:
+	os.system(f'C:/steamcmd/steamcmd.exe +force_install_dir C:/ArmA 3/Arma 3 Server +login anonymous +workshop_download_item 107410 {modDict[key]} +quit')
 	os.rename(MOD_DIR + mod, MOD_DIR + "@" + editedModName)
