9b2e4016f30b94a0b65c49d0789502129114554b
ada taylor <admin@adataylor.io>
2024-06-12 02:02:43 -0500

    moved around contents of if/else

--- modDownloader.py
+++ modDownloader.py
@@ -10,9 +10,9 @@ def ModDownloadAndRename(mod_id, mod_name):
 	if os.path.isdir(MOD_DIR + "@" + mod_name):
 		print("mod exists.. skipping")
 	else:
-		os.rename(SOURCE_DIR + mod_id, MOD_DIR + "@" + mod_name)
 		try:
 			subprocess.run(f'C:/steamcmd/steamcmd.exe +force_install_dir ../ArmA 3/Arma 3 Server/ +login anonymous +workshop_download_item 107410 {item[1]} +quit')
+			os.rename(SOURCE_DIR + mod_id, MOD_DIR + "@" + mod_name)
 		except Exception as e:
 			raise(e)
 		
