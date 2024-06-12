2a74196fcf9fca2b84eb2a54d2a0413f34bd6dcf
ada taylor <admin@adataylor.io>
2024-06-12 01:36:49 -0500

    added error checking

--- modDownloader.py
+++ modDownloader.py
@@ -6,10 +6,11 @@ MOD_DIR = "C:/ArmA 3/Arma 3 Server/"
 
 def ModDownloadAndRename(mod_id, mod_name):
 	subprocess.run(f'C:/steamcmd/steamcmd.exe +force_install_dir ../ArmA 3/Arma 3 Server/ +login anonymous +workshop_download_item 107410 {item[1]} +quit')
-	try:
+	if os.path.isdir(MOD_DIR + "@" + "mod_name"):
+		print("mod exists.. skipping")
+	else:
 		os.rename(SOURCE_DIR + mod_id, MOD_DIR + "@" + mod_name)
-	except Exception as e:
-		raise e
+		
 with open('mods.html') as f:
 	read_data = f.read()
 
