863b7dbc677bab094627593c422796f5dee27802
ada taylor <admin@adataylor.io>
2024-06-12 04:42:48 -0500

    typo

--- modDownloader.py
+++ modDownloader.py
@@ -5,7 +5,7 @@ import re, os, subprocess, time, requests
 load_dotenv()
 
 USER_NAME = os.getenv("USER_NAME")
-PASSWORD = os.getnev("PASSWORD")
+PASSWORD = os.getenv("PASSWORD")
 SOURCE_DIR = "C:/arma/steamapps/workshop/content/107410/"
 MOD_DIR = "C:/ArmA 3/Arma 3 Server/"
 
