1dacc881feb7cdbdae185de633505373e539fb4e
ada taylor <admin@adataylor.io>
2024-06-12 02:17:46 -0500

    updated logic

--- modDownloader.py
+++ modDownloader.py
@@ -7,11 +7,10 @@ MOD_DIR = "C:/ArmA 3/Arma 3 Server/"
 def ModDownloadAndRename(mod_id, mod_name):
 	if os.path.isdir(MOD_DIR + "@" + mod_name):
 		print("mod exists.. skipping")
-	elif not os.path.isdir(SOURCE_DIR + mod_id):
-		subprocess.run(f'C:/steamcmd/steamcmd.exe +force_install_dir ../ArmA 3/Arma 3 Server/ +login anonymous +workshop_download_item 107410 {item[1]} +quit')
-		os.rename(SOURCE_DIR + mod_id, MOD_DIR + "@" + mod_name)
 	else:
-		ModDownloadAndRename(mod_id, mod_name)
+		while not os.path.isdir(SOURCE_DIR + mod_id):
+			subprocess.run(f'C:/steamcmd/steamcmd.exe +force_install_dir ../ArmA 3/Arma 3 Server/ +login anonymous +workshop_download_item 107410 {item[1]} +quit')
+		os.rename(SOURCE_DIR + mod_id, MOD_DIR + "@" + mod_name)
 		
 with open('mods.html') as f:
 	read_data = f.read()
