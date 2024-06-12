666f15ae52f286a98156323c86469503cbba63d6
ada taylor <admin@adataylor.io>
2024-06-12 01:04:53 -0500

    updated to use tenacity

--- modDownloader.py
+++ modDownloader.py
@@ -1,5 +1,15 @@
 from bs4 import BeautifulSoup
 import re, os, subprocess
+from tenacity import retry, stop_after_attempt, wait_fixed
+
+@retry(wait=wait_fixed(60))
+def ModDownloadAndRename(mod_id, mod_name):
+	try:
+		subprocess.run(f'C:/steamcmd/steamcmd.exe +force_install_dir ../ArmA 3/Arma 3 Server/ +login anonymous +workshop_download_item 107410 {item[1]} +quit')
+		os.rename(MOD_DIR + mod_id, MOD_DIR + "@" + mod_name)
+		return 0
+	except: Exception as e:
+		raise(e)
 
 with open('mods.html') as f:
 	read_data = f.read()
@@ -24,5 +34,4 @@ for link in soup.find_all('a'):
 modDict = {key: value for key, value in zip(modListName, modListID)}
 
 for item in modDict.items():
-	subprocess.run(f'C:/steamcmd/steamcmd.exe +force_install_dir C:/ArmA 3/Arma 3 Server/ +login anonymous +workshop_download_item 107410 {item[1]} +quit')
-	os.rename(MOD_DIR + item[1], MOD_DIR + "@" + item[0])
+	ModDownloadAndRename(item[1], item[0])
